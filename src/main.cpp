d97a9272d2d293b90000cdf24f10530f27040413
Samyak404 <99331321+Samyak404@users.noreply.github.com>
2022-03-27 16:36:48 +0530

    Revert "rough"
    This reverts commit 2a1c889b9fda09aacf72d148a065d3b75bfa0a7a.

--- src/main.cpp
+++ src/main.cpp
@@ -32,100 +32,27 @@ const int servoPinNumber2 = 12;
 // Define Firebase Data For Servo Motor
 FirebaseData servoMotor;
 
+// Define Firebase Data For 1st Dc motor
+FirebaseData motor1Rotation;
+FirebaseData motor1Speed;
+FirebaseData motor1State;
 
-
-
-
-
-
-// Testing Methods
-
-unsigned long sendDataPrevMillis = 0;
-int count = 0;
-volatile bool dataChanged = false;
-
-
-FirebaseData fbdo, stream;
-
-String parentPath = "/DC Motor/Motor 1";
-String childPath[3] = {"/Rotation", "/Speed", "/State"};
-
-
-
-
-
-
-
-
-
-
-
-
-void streamCallback(MultiPathStream stream)
-{
-  size_t numChild = sizeof(childPath) / sizeof(childPath[0]);
-
-  for (size_t i = 0; i < numChild; i++)
-  {
-    if (stream.get(childPath[i]))
-    {
-      Serial.printf("path: %s, event: %s, type: %s, value: %s%s", stream.dataPath.c_str(), stream.eventType.c_str(), stream.type.c_str(), stream.value.c_str(), i < numChild - 1 ? "\n" : "");
-    }
-  }
-
-  Serial.println();
-
-  //This is the size of stream payload received (current and max value)
-  //Max payload size is the payload size under the stream path since the stream connected
-  //and read once and will not update until stream reconnection takes place.
-  //This max value will be zero as no payload received in case of ESP8266 which
-  //BearSSL reserved Rx buffer size is less than the actual stream payload.
-  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", stream.payloadLength(), stream.maxPayloadLength());
-
-  //Due to limited of stack memory, do not perform any task that used large memory here especially starting connect to server.
-  //Just set this flag and check it status later.
-  dataChanged = true;
-}
-
-void streamTimeoutCallback(bool timeout)
-{
-  if (timeout)
-    Serial.println("stream timed out, resuming...\n");
-
-  if (!stream.httpConnected())
-    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
-}
-
-
-
-
-
-
-
-
-
+//Define pinout for 1st Dc motor. Pin Numbers For Arduino Framework
+const int motor1PWM = 32;
+const int channel1 = 4;
+const int m1CwDirection = 23;
+const int m1CcwDirection = 22;
 
 // Define Firebase Data For 1st Dc motor
-// FirebaseData motor1Rotation;
-// FirebaseData motor1Speed;
-// FirebaseData motor1State;
+FirebaseData motor2Rotation;
+FirebaseData motor2Speed;
+FirebaseData motor2State;
 
-// //Define pinout for 1st Dc motor. Pin Numbers For Arduino Framework
-// const int motor1PWM = 32;
-// const int channel1 = 4;
-// const int m1CwDirection = 23;
-// const int m1CcwDirection = 22;
-
-// // Define Firebase Data For 2st Dc motor
-// FirebaseData motor2Rotation;
-// FirebaseData motor2Speed;
-// FirebaseData motor2State;
-
-// //Define pinout for 2st Dc motor. Pin Numbers For Arduino Framework
-// const int motor2PWM = 33;
-// const int channel2 = 5;
-// const int m2CwDirection = 3;
-// const int m2CcwDirection = 21;
+//Define pinout for 2st Dc motor. Pin Numbers For Arduino Framework
+const int motor2PWM = 33;
+const int channel2 = 5;
+const int m2CwDirection = 3;
+const int m2CcwDirection = 21;
 
 FirebaseAuth auth;
 FirebaseConfig config;
@@ -177,27 +104,27 @@ void setup() {
     {Serial.printf("stream begin error, %s\n\n", servoMotor.errorReason().c_str());}
   
 
-// // For 1st DC Motor
-//   if (!Firebase.RTDB.beginStream(&motor1Rotation, "/DC Motor/Motor 1/Rotation"))
-//     {Serial.printf("sream begin error, %s\n\n", motor1Rotation.errorReason().c_str());}
+// For 1st DC Motor
+  if (!Firebase.RTDB.beginStream(&motor1Rotation, "/DC Motor/Motor 1/Rotation"))
+    {Serial.printf("sream begin error, %s\n\n", motor1Rotation.errorReason().c_str());}
 
-//   if (!Firebase.RTDB.beginStream(&motor1Speed, "/DC Motor/Motor 1/Speed"))
-//     {Serial.printf("sream begin error, %s\n\n", motor1Speed.errorReason().c_str());}
+  if (!Firebase.RTDB.beginStream(&motor1Speed, "/DC Motor/Motor 1/Speed"))
+    {Serial.printf("sream begin error, %s\n\n", motor1Speed.errorReason().c_str());}
 
-//   if (!Firebase.RTDB.beginStream(&motor1State, "/DC Motor/Motor 1/State"))
-//     {Serial.printf("sream begin error, %s\n\n", motor1State.errorReason().c_str());}
+  if (!Firebase.RTDB.beginStream(&motor1State, "/DC Motor/Motor 1/State"))
+    {Serial.printf("sream begin error, %s\n\n", motor1State.errorReason().c_str());}
 
 
 
-// // For 2st DC Motor
-//   if (!Firebase.RTDB.beginStream(&motor2Rotation, "/DC Motor/Motor 2/Rotation"))
-//     {Serial.printf("sream begin error, %s\n\n", motor2Rotation.errorReason().c_str());}
+// For 2st DC Motor
+  if (!Firebase.RTDB.beginStream(&motor2Rotation, "/DC Motor/Motor 2/Rotation"))
+    {Serial.printf("sream begin error, %s\n\n", motor2Rotation.errorReason().c_str());}
 
-//   if (!Firebase.RTDB.beginStream(&motor2Speed, "/DC Motor/Motor 2/Speed"))
-//     {Serial.printf("sream begin error, %s\n\n", motor2Speed.errorReason().c_str());}
+  if (!Firebase.RTDB.beginStream(&motor2Speed, "/DC Motor/Motor 2/Speed"))
+    {Serial.printf("sream begin error, %s\n\n", motor2Speed.errorReason().c_str());}
 
-//   if (!Firebase.RTDB.beginStream(&motor2State, "/DC Motor/Motor 2/State"))
-//     {Serial.printf("sream begin error, %s\n\n", motor2State.errorReason().c_str());}
+  if (!Firebase.RTDB.beginStream(&motor2State, "/DC Motor/Motor 2/State"))
+    {Serial.printf("sream begin error, %s\n\n", motor2State.errorReason().c_str());}
 
 
   // For Servo motors
@@ -212,27 +139,21 @@ void setup() {
 
   // DC Motor Decleration
 
-  // // For DC motor PWM 1
-  // ledcSetup(channel1, frequency, resolution);
-  // ledcAttachPin(motor1PWM, channel1);
+  // For DC motor PWM 1
+  ledcSetup(channel1, frequency, resolution);
+  ledcAttachPin(motor1PWM, channel1);
 
-  // pinMode(m1CwDirection, OUTPUT);
-  // pinMode(m1CcwDirection, OUTPUT);
+  pinMode(m1CwDirection, OUTPUT);
+  pinMode(m1CcwDirection, OUTPUT);
 
 
-  // // For DC motor PWM 2
-  // ledcSetup(channel2, frequency, resolution);
-  // ledcAttachPin(motor2PWM, channel2);
+  // For DC motor PWM 2
+  ledcSetup(channel2, frequency, resolution);
+  ledcAttachPin(motor2PWM, channel2);
 
-  // pinMode(m2CwDirection, OUTPUT);
-  // pinMode(m2CcwDirection, OUTPUT);
+  pinMode(m2CwDirection, OUTPUT);
+  pinMode(m2CcwDirection, OUTPUT);
 
-  Firebase.RTDB.beginStream(&stream, parentPath);
-
-  // if (!Firebase.RTDB.beginMultiPathStream(&stream, parentPath))
-  //   Serial.printf("sream begin error, %s\n\n", stream.errorReason().c_str());
-
-  // Firebase.RTDB.setMultiPathStreamCallback(&stream, streamCallback, streamTimeoutCallback);
 }
 
 void loop() {
@@ -257,37 +178,37 @@ void loop() {
       }
     }
 
-// //  For DC Motor 1
-//       Firebase.RTDB.readStream(&motor1Rotation);
-//       motor1Rotation.httpConnected();
+//  For DC Motor 1
+      Firebase.RTDB.readStream(&motor1Rotation);
+      motor1Rotation.httpConnected();
 
-//       Firebase.RTDB.readStream(&motor1Speed);
-//       motor1Speed.httpConnected();
+      Firebase.RTDB.readStream(&motor1Speed);
+      motor1Speed.httpConnected();
 
-//       Firebase.RTDB.readStream(&motor1State);
-//       motor1State.httpConnected();
+      Firebase.RTDB.readStream(&motor1State);
+      motor1State.httpConnected();
 
 
-//     if (motor1State.intData() == 1)
-//     {
-//       if (motor1Rotation.intData() == 1)
-//       {
-//         digitalWrite(m1CwDirection, LOW);
-//         digitalWrite(m1CcwDirection, HIGH);
-//         ledcWrite(channel1, motor1Speed.intData() * 255 / 100);
-//       }
-//       else if (motor1Rotation.intData() == 0)
-//       {
-//         digitalWrite(m1CcwDirection, LOW);
-//         digitalWrite(m1CwDirection, HIGH);
-//         ledcWrite(channel1, motor1Speed.intData() * 255 / 100);
-//       }
-//     }
-//     else if (motor1State.intData() == 0)
-//     {
-//       digitalWrite(m1CcwDirection, LOW);
-//       digitalWrite(m1CwDirection, LOW);
-//     }
+    if (motor1State.intData() == 1)
+    {
+      if (motor1Rotation.intData() == 1)
+      {
+        digitalWrite(m1CwDirection, LOW);
+        digitalWrite(m1CcwDirection, HIGH);
+        ledcWrite(channel1, motor1Speed.intData() * 255 / 100);
+      }
+      else if (motor1Rotation.intData() == 0)
+      {
+        digitalWrite(m1CcwDirection, LOW);
+        digitalWrite(m1CwDirection, HIGH);
+        ledcWrite(channel1, motor1Speed.intData() * 255 / 100);
+      }
+    }
+    else if (motor1State.intData() == 0)
+    {
+      digitalWrite(m1CcwDirection, LOW);
+      digitalWrite(m1CwDirection, LOW);
+    }
 
 
 
@@ -322,15 +243,5 @@ void loop() {
     //   digitalWrite(m2CcwDirection, LOW);
     //   digitalWrite(m2CwDirection, LOW);
     // }
-
-
-     if (dataChanged)
-    {
-      dataChanged = false;
-      //When stream data is available, do anything here...
-
-    }
-
-
   }
 }
--- test/roughWork.cpp
+++ test/roughWork.cpp
@@ -16,8 +16,6 @@
 #define WIFI_SSID "WIFI_AP"
 #define WIFI_PASSWORD "WIFI_PASSWORD"
 
-//For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
-
 /* 2. Define the API Key */
 #define API_KEY "API_KEY"
 
@@ -30,54 +28,15 @@
 
 //Define Firebase Data object
 FirebaseData fbdo;
-FirebaseData stream;
 
 FirebaseAuth auth;
 FirebaseConfig config;
 
 unsigned long sendDataPrevMillis = 0;
 
-String parentPath = "/test/stream/data";
-String childPath[2] = {"/node1", "/node2"};
-
 int count = 0;
 
-volatile bool dataChanged = false;
-
-void streamCallback(MultiPathStream stream)
-{
-  size_t numChild = sizeof(childPath) / sizeof(childPath[0]);
-
-  for (size_t i = 0; i < numChild; i++)
-  {
-    if (stream.get(childPath[i]))
-    {
-      Serial.printf("path: %s, event: %s, type: %s, value: %s%s", stream.dataPath.c_str(), stream.eventType.c_str(), stream.type.c_str(), stream.value.c_str(), i < numChild - 1 ? "\n" : "");
-    }
-  }
-
-  Serial.println();
-
-  //This is the size of stream payload received (current and max value)
-  //Max payload size is the payload size under the stream path since the stream connected
-  //and read once and will not update until stream reconnection takes place.
-  //This max value will be zero as no payload received in case of ESP8266 which
-  //BearSSL reserved Rx buffer size is less than the actual stream payload.
-  Serial.printf("Received stream payload size: %d (Max. %d)\n\n", stream.payloadLength(), stream.maxPayloadLength());
-
-  //Due to limited of stack memory, do not perform any task that used large memory here especially starting connect to server.
-  //Just set this flag and check it status later.
-  dataChanged = true;
-}
-
-void streamTimeoutCallback(bool timeout)
-{
-  if (timeout)
-    Serial.println("stream timed out, resuming...\n");
-
-  if (!stream.httpConnected())
-    Serial.printf("error code: %d, reason: %s\n\n", stream.httpCode(), stream.errorReason().c_str());
-}
+uint32_t idleTimeForStream = 15000;
 
 void setup()
 {
@@ -97,6 +56,8 @@ void setup()
   Serial.println();
 
   Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+  //For the following credentials, see examples/Authentications/SignInAsUser/EmailPassword/EmailPassword.ino
 
   /* Assign the api key (required) */
   config.api_key = API_KEY;
@@ -121,77 +82,45 @@ void setup()
 
   Firebase.reconnectWiFi(true);
 
-  //Recommend for ESP8266 stream, adjust the buffer size to match your stream data size
-#if defined(ESP8266)
-  stream.setBSSLBufferSize(2048 /* Rx in bytes, 512 - 16384 */, 512 /* Tx in bytes, 512 - 16384 */);
-#endif
-
   //The data under the node being stream (parent path) should keep small
   //Large stream payload leads to the parsing error due to memory allocation.
-
-  //The MultiPathStream works as normal stream with the payload parsing function.
-
-  if (!Firebase.RTDB.beginMultiPathStream(&stream, parentPath))
-    Serial.printf("sream begin error, %s\n\n", stream.errorReason().c_str());
-
-  Firebase.RTDB.setMultiPathStreamCallback(&stream, streamCallback, streamTimeoutCallback);
-
-  /** Timeout options, below is default config.
-
-  //WiFi reconnect timeout (interval) in ms (10 sec - 5 min) when WiFi disconnected.
-  config.timeout.wifiReconnect = 10 * 1000;
-
-  //Socket begin connection timeout (ESP32) or data transfer timeout (ESP8266) in ms (1 sec - 1 min).
-  config.timeout.socketConnection = 30 * 1000;
-
-  //ESP32 SSL handshake in ms (1 sec - 2 min). This option doesn't allow in ESP8266 core library.
-  config.timeout.sslHandshake = 2 * 60 * 1000;
-
-  //Server response read timeout in ms (1 sec - 1 min).
-  config.timeout.serverResponse = 10 * 1000;
-
-  //RTDB Stream keep-alive timeout in ms (20 sec - 2 min) when no server's keep-alive event data received.
-  config.timeout.rtdbKeepAlive = 45 * 1000;
-
-  //RTDB Stream reconnect timeout (interval) in ms (1 sec - 1 min) when RTDB Stream closed and want to resume.
-  config.timeout.rtdbStreamReconnect = 1 * 1000;
-
-  //RTDB Stream error notification timeout (interval) in ms (3 sec - 30 sec). It determines how often the readStream
-  //will return false (error) when it called repeatedly in loop.
-  config.timeout.rtdbStreamError = 3 * 1000;
-
-  */
+  if (!Firebase.RTDB.beginStream(&fbdo, "/test/stream/data"))
+    Serial.printf("sream begin error, %s\n\n", fbdo.errorReason().c_str());
 }
 
 void loop()
 {
 
-  if (Firebase.ready() && (millis() - sendDataPrevMillis > 15000 || sendDataPrevMillis == 0))
+  if (Firebase.ready() && (millis() - sendDataPrevMillis > idleTimeForStream || sendDataPrevMillis == 0))
   {
     sendDataPrevMillis = millis();
-
-    Serial.print("\nSet json...");
-
-    FirebaseJson json;
-
-    for (size_t i = 0; i < 10; i++)
-    {
-      json.set("node1/data", "v1");
-      json.set("node1/num", count);
-      json.set("node2/data", "v2");
-      json.set("node2/num", count * 3);
-      //The response is ignored in this async function, it may return true as long as the connection is established.
-      //The purpose for this async function is to set, push and update data instantly.
-      Firebase.RTDB.setJSONAsync(&fbdo, parentPath, &json);
-      count++;
-    }
-
-    Serial.println("ok\n");
+    count++;
+    //Due to single FirebaseData object used, stream connection will be interruped to send/receive data
+    Serial.printf("Set string... %s\n\n", Firebase.RTDB.setString(&fbdo, "/test/stream/data", "Hello World! " + String(count)) ? "ok" : fbdo.errorReason().c_str());
   }
 
-  if (dataChanged)
+  if (Firebase.ready())
   {
-    dataChanged = false;
-    //When stream data is available, do anything here...
+
+    if (!Firebase.RTDB.readStream(&fbdo))
+      Serial.printf("sream read error, %s\n\n", fbdo.errorReason().c_str());
+
+    if (fbdo.streamTimeout())
+    {
+      Serial.println("stream timed out, resuming...\n");
+
+      if (!fbdo.httpConnected())
+        Serial.printf("error code: %d, reason: %s\n\n", fbdo.httpCode(), fbdo.errorReason().c_str());
+    }
+
+    if (fbdo.streamAvailable())
+    {
+      Serial.printf("sream path, %s\nevent path, %s\ndata type, %s\nevent type, %s\nvalue, %s\n\n",
+                    fbdo.streamPath().c_str(),
+                    fbdo.dataPath().c_str(),
+                    fbdo.dataType().c_str(),
+                    fbdo.eventType().c_str(),
+                    fbdo.stringData().c_str());
+    }
   }
 }
